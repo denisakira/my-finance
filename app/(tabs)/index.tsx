d2dbb3bcf6a7429d2ee977f645c415a2513812d4
Denis Akira Ise Washio <denis@readyon.ai>
2023-12-11 00:55:15 -0300

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Card, CardHeader, H5, Stack, Text, View } from 'tamagui';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Card elevate size="$5" bordered width={'100%'}>
+        <CardHeader padded>
+          <H5>In</H5>
+        </CardHeader>
+
+        <Stack margin={20}>
+          <Text color="$color">$3000</Text>
+        </Stack>
+      </Card>
     </View>
   );
 }
@@ -16,16 +21,9 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff5e4',
+    padding: 40,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    justifyContent: 'flex-start',
   },
 });
